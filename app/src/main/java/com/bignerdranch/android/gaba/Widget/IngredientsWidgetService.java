40011390156f36db397e4390ac1f86502503a014
mbeevor <mbeevor@gmail.com>
2018-06-17 19:37:34 +0100

    Third attempt part two

--- app/src/main/java/com/bignerdranch/android/gaba/Widget/IngredientsWidgetService.java
+++ app/src/main/java/com/bignerdranch/android/gaba/Widget/IngredientsWidgetService.java
@@ -11,103 +11,86 @@ import com.bignerdranch.android.gaba.R;
 
 import java.util.ArrayList;
 
-import static com.bignerdranch.android.gaba.Model.Keys.INGREDIENTS_LIST;
-import static com.bignerdranch.android.gaba.Model.Keys.RECIPE_NAME;
-import static com.bignerdranch.android.gaba.Model.Keys.STEPS_LIST;
-
 /**
  * Created by Matthew on 16/06/2018.
  */
 
 
-public class IngredientsWidgetService implements RemoteViewsService.RemoteViewsFactory {
+public class IngredientsWidgetService extends RemoteViewsService {
 
     private Context context;
     private String recipeName;
     private ArrayList<Ingredients> ingredientsList;
     private ArrayList<Steps> stepsList;
 
-    IngredientsWidgetService(Context applicationContext, Intent intent) {
-        context = applicationContext;
-        recipeName = intent.getStringExtra(RECIPE_NAME);
-        ingredientsList = intent.getParcelableArrayListExtra(INGREDIENTS_LIST);
-        stepsList = intent.getParcelableArrayListExtra(STEPS_LIST);
-        loadIngredients();
+    @Override
+    public RemoteViewsFactory onGetViewFactory(Intent intent) {
+        return new IngredientsListViewRemoteViewsFactory(this.getApplicationContext());
     }
 
-    public class IngredientsViewsService extends RemoteViewsService {
+    private class IngredientsListViewRemoteViewsFactory implements RemoteViewsFactory {
+        public IngredientsListViewRemoteViewsFactory(Context applicationContext) {
+        }
 
         @Override
-        public RemoteViewsFactory onGetViewFactory(Intent intent) {
+        public void onCreate() {
 
-            return new IngredientsWidgetService(this.getApplicationContext(), intent);
         }
-    }
 
-    private void loadIngredients() {
+        @Override
+        public void onDataSetChanged() {
 
-
-    }
-
-    @Override
-    public void onCreate() {
-
-    }
-
-    @Override
-    public void onDataSetChanged() {
-
-    }
-
-    @Override
-    public void onDestroy() {
-        if (ingredientsList != null) {
-            ingredientsList = null;
         }
-    }
 
+        @Override
+        public void onDestroy() {
 
-    @Override
-    public int getCount() {
-        if (ingredientsList != null) return ingredientsList.size();
-        else return 0;
-    }
+        }
 
-    @Override
-    public RemoteViews getViewAt(int position) {
-        if (ingredientsList == null) {
+        @Override
+        public int getCount() {
+            if (ingredientsList != null) return ingredientsList.size();
+            else return 0;
+        }
+
+        @Override
+        public RemoteViews getViewAt(int position) {
+            if (ingredientsList == null) {
+                return null;
+            } else {
+                RemoteViews remoteViews = new RemoteViews(context.getPackageName(), R.layout.widget_list_item);
+                Ingredients ingredients = ingredientsList.get(position);
+
+                String ingredientName = ingredients.getItemIngredient();
+                String ingredientQuantity = ingredients.getItemQuantity();
+                String ingredientMeasure = ingredients.getItemMeasure();
+                String ingredientString = ingredientName + "" + ingredientQuantity + "" + ingredientMeasure;
+
+                remoteViews.setTextViewText(R.id.widget_list_item_tv, ingredientString);
+                return remoteViews;
+            }
+        }
+
+        @Override
+        public RemoteViews getLoadingView() {
             return null;
-        } else {
-            RemoteViews remoteViews = new RemoteViews(context.getPackageName(), R.layout.widget_list_item);
-            Ingredients ingredients = ingredientsList.get(position);
-
-            String ingredientName = ingredients.getItemIngredient();
-            String ingredientQuantity = ingredients.getItemQuantity();
-            String ingredientMeasure = ingredients.getItemMeasure();
-            String ingredientString = ingredientName + "" + ingredientQuantity + "" + ingredientMeasure;
-
-            remoteViews.setTextViewText(R.id.widget_list_item_tv, ingredientString);
-            return remoteViews;
         }
-    }
 
-    @Override
-    public RemoteViews getLoadingView() {
-        return null;
-    }
+        @Override
+        public int getViewTypeCount() {
+            return 0;
+        }
 
-    @Override
-    public int getViewTypeCount() {
-        return 0;
-    }
+        @Override
+        public long getItemId(int i) {
+            return 0;
+        }
 
-    @Override
-    public long getItemId(int position) {
-        return position;
-    }
-
-    @Override
-    public boolean hasStableIds() {
-        return true;
+        @Override
+        public boolean hasStableIds() {
+            return false;
+        }
     }
 }
+
+
